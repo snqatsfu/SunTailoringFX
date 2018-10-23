57a1d4d30f886a26cb2bad31f0d312ba685015c1
nzheng <nathan.zheng@kardium.com>
2018-10-23 15:46:56 -0700

    Added generate fake invoices for a year script

---
+++ src/scripts/GenerateFakeInvoicesOneYear.java
@@ -0,0 +1,42 @@
+package scripts;
+
+import Data.CustomerInfo;
+import Data.Invoice;
+import Data.Item;
+import GUI.InvoiceStore;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * TODO: CLASS JAVA DOC HERE
+ */
+public class GenerateFakeInvoicesOneYear {
+    public static void main(String[] args) {
+        final InvoiceStore store = InvoiceStore.getInstance();
+
+        int count = 0;
+        final LocalDate today = LocalDate.now();
+        final Random random = new Random();
+        for (int i = 0; i < 365; i++) {
+            final LocalDate invoiceDate = today.minusDays(i);
+            final int numInvoicesThisDay = random.nextInt(10);
+            for (int j = 0; j < numInvoicesThisDay; j++) {
+                String invoiceNumber = invoiceDate + "_" + j;
+                final LocalDate dueDate = invoiceDate.plusDays(new Random().nextInt(7));
+                final int numItems = random.nextInt(5);
+                List<Item> items = new ArrayList<>(numItems);
+                for (int k = 0; k < numItems; k++) {
+                    items.add(new Item("Fake Item", random.nextInt(3), random.nextInt(20)));
+                }
+                store.save(new Invoice(invoiceNumber, invoiceDate, dueDate, CustomerInfo.getSAMPLE(), items, 0, false, false, false));
+                count++;
+            }
+        }
+
+        System.out.println("Generated " + count + " fake invoices");
+    }
+
+}
